e909f8abcf6d1b21a1fe1ef0b8663ebaf92fa716
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2025-05-02 18:07:55 -0400

    Create Ejercicio_22.cpp

---
+++ Ejercicio_22.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int f, c
+    cout << "ingrese número de filas: "
+    cin >> f
+    cout << "ingrese número de columnas: "
+    cin >> c
+
+    int a[10][10], b[10][10]
+    int i = 0
+    while(i < f)
+    {
+        int j = 0
+        while(j < c)
+        {
+            cout << "a[" << i << "][" << j << "]: "
+            cin >> a[i][j]
+            j++
+        }
+        i++
+    }
+
+    i = 0
+    while(i < f)
+    {
+        int j = 0
+        while(j < c)
+        {
+            b[i][j] = a[i][j]
+            j++
+        }
+        i++
+    }
+
+    cout << "matriz copiada:" << endl
+    i = 0
+    while(i < f)
+    {
+        int j = 0
+        while(j < c)
+        {
+            cout << b[i][j] << " "
+            j++
+        }
+        cout << endl
+        i++
+    }
+          }
