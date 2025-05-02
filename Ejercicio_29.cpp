b607a5a163583232a6ab0ed9660ef89d8dbba3e0
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2025-05-02 18:11:40 -0400

    Create Ejercicio_29.cpp

---
+++ Ejercicio_29.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int f, c
+    cout << "ingrese número de filas: "
+    cin >> f
+    cout << "ingrese número de columnas: "
+    cin >> c
+
+    int m[10][10]
+    int i = 0
+    while(i < f)
+    {
+        int j = 0
+        while(j < c)
+        {
+            cout << "m[" << i << "][" << j << "]: "
+            cin >> m[i][j]
+            j++
+        }
+        i++
+    }
+
+    int negativos = 0
+    i = 0
+    while(i < f)
+    {
+        int j = 0
+        while(j < c)
+        {
+            if(m[i][j] < 0)
+                negativos++
+            j++
+        }
+        i++
+    }
+
+    cout << "cantidad de elementos negativos: " << negativos << endl
+    }
