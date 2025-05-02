80914407f0fafd624d2fca37e07c5a17143df308
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2025-05-02 17:57:02 -0400

    Create Ejercicio_02.cpp

---
+++ Ejercicio_02.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+{
+void mostrarTabla(int num)
+    for(int i = 1; i <= 10; i++)
+        cout << num << " x " << i << " = " << num * i << endl
+}
+
+void mostrarTablasDesde2(int limite)
+{
+    for(int i = 2; i <= limite; i++)
+        mostrarTabla(i)
+        cout << end;
+}
+
+int main()
+{
+    int n
+    cout << "Ingrese un número: "
+    cin >> n
+    mostrarTablasDesde2(n)
+}
